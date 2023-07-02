29258a447b005124bbe43e53237bf07de73938f9
koomekevo <koome.kevo@gurucrafts.net>
2023-07-02 14:41:50 +0300

    Add Login.js component

---
+++ src/components/Login.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Perform login logic
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="email" placeholder="Email" value={email} onChange={handleEmailChange} />
+        <input type="password" placeholder="Password" value={password} onChange={handlePasswordChange} />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
