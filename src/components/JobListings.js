590bb2bbab0a0629c606c67f4c6f6d2ed9e2009a
koomekevo <koome.kevo@gurucrafts.net>
2023-07-02 14:50:03 +0300

    Add JobListings component

---
+++ src/components/JobListings.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+const JobListings = () => {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('/api/jobs')
+      .then((response) => {
+        setJobs(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>Job Listings</h2>
+      {jobs.map((job) => (
+        <div key={job._id}>
+          <h3>{job.title}</h3>
+          <p>{job.company}</p>
+          <Link to={`/jobs/${job._id}`}>View Details</Link>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default JobListings;
