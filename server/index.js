8af1e61423be785b5cc31609bc399d22937bcbc4
koomekevo <koome.kevo@gurucrafts.net>
2023-07-02 14:28:32 +0300

    Modify index.js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Connect to MongoDB
-mongoose.connect("mongodb://localhost/vacanciestoday", {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
@@ -19,6 +23,12 @@ connection.once("open", () => {
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// Routes
+const jobsRouter = require("./routes/jobs");
+const authRouter = require("./routes/auth");
+app.use("/api/jobs", jobsRouter);
+app.use("/api/auth", authRouter);
 
 // Start the server
 app.listen(PORT, () => {
