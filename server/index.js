f5bb48b929b5918a66d13489bf7dc8782bc3e076
koomekevo <koome.kevo@gurucrafts.net>
2023-07-02 22:03:38 +0300

    Fix JobListings error

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const jobListingsRouter = require("./routes/jobListings");
+const jobListingsRouter = require("./routes/JobListings");
 const authRouter = require("./routes/auth");
 
 dotenv.config();
